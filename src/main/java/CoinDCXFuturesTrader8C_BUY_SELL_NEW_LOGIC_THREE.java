b6750bc138e4268214f62bff5176f4dd84682ace
Ankit <87983876+ankitkmp98@users.noreply.github.com>
2025-05-14 23:38:00 +0530

    Update CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE.java

--- src/main/java/CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE.java
+++ src/main/java/CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE.java
@@ -46,7 +46,8 @@ public class CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE {
             "MOVE", "MUBARAK", "NEIRO", "NOT", "ONDO", "OP", "PEOPLE", "PEPE", "PENGU",
             "PI", "PNUT", "POL", "POPCAT", "RARE", "RED", "RSR", "SAGA", "SAND", "SEI",
             "SHIB", "SOLV", "SONIC", "SPX", "STX", "SUN", "SWARMS", "SUSHI", "TST", "TRX",
-            "USUAL", "VINE", "VIRTUAL", "WIF", "WLD", "XAI", "XLM", "XRP", "ZK"
+            "USUAL", "VINE", "VIRTUAL", "WIF", "WLD", "XAI", "XLM", "XRP", "ZK","TAO",
+            "TRUMP","PERP","OM","BNB","LINK","GMT","LAYER"
     };
 
     private static final Set<String> INTEGER_QUANTITY_PAIRS = Stream.of(COIN_SYMBOLS)
