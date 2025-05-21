a400ca65f1532f7d3b5130ffc47b4e8ae2d2a982
Ankit <87983876+ankitkmp98@users.noreply.github.com>
2025-05-21 08:30:37 +0530

    Update CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE.java

--- src/main/java/CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE.java
+++ src/main/java/CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE.java
@@ -338,7 +338,7 @@ public class CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE {
             double conversionRate = 93.0; // USDT to INR conversion rate
 
             double tpPercentage = 0.02; // 50% of margin for TP
-            double slPercentage = 0.05; // 8% of margin for SL
+            double slPercentage = 0.08; // 8% of margin for SL
 
             // double tpPercentage = 0.50; // 50% of margin for TP
             // double slPercentage = 0.05; // 8% of margin for SL
