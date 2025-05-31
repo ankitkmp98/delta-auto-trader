401ae7d93b8f582741d2e9fba209afe00a4f0f5e
Ankit <87983876+ankitkmp98@users.noreply.github.com>
2025-05-31 23:37:22 +0530

    Update CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE.java

--- src/main/java/CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE.java
+++ src/main/java/CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE.java
@@ -318,8 +318,8 @@ public class CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE {
 
 
 
-        double tpPercentage = 0.60;
-        double slPercentage = 0.20;
+        double tpPercentage = 0.10;
+        double slPercentage = 0.25;
 
 
 
