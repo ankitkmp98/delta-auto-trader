660024cd43f74e47471740b4af3c6c18229151dd
Ankit <87983876+ankitkmp98@users.noreply.github.com>
2026-07-17 21:48:54 +0530

    Update CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE.java

--- src/main/java/CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE.java
+++ src/main/java/CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE.java
@@ -70,7 +70,7 @@ public class CoinDCXFuturesTrader8C_BUY_SELL_NEW_LOGIC_THREE {
     private static final String BASE_URL       = "https://api.coindcx.com";
     private static final String PUBLIC_API_URL = "https://public.coindcx.com";
 
-    private static final double MAX_MARGIN = 200.0;
+    private static final double MAX_MARGIN = 60.0;
     private static final int    LEVERAGE   = 15;
 
     private static final int MAX_ENTRY_PRICE_CHECKS = 20;
